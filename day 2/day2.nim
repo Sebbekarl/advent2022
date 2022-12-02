20cbbe6791ec40a6c0f00fa12ac1a2bdd6a81222
sebbekarl <sebbekarl97@gmail.com>
2022-12-02 15:09:52 +0100

    day2

--- day 2/day2.nim
+++ day 2/day2.nim
@@ -1,27 +1,17 @@
-import std/tables
-from std/sequtils import zip
-# r p s
-# 1 2 3
-# L D W
-# 0 3 6
-let
-    games = ["A X","A Y","A Z","B X","B Y","B Z","C X","C Y","C Z"]
-    #outcome = [3+1,6+2,0+3,0+1,3+2,6+3,6+1,0+2,3+3]
-    outcome = [0+3,3+1,6+2,0+1,3+2,6+3,0+2,3+3,6+1]
-    input = open("input.txt")
-var 
-  lookup = initTable[string,int]()
-  sum = 0
+import tables
 
-
-for pairs in zip(games, outcome):
-  let (games, outcome) = pairs
-  lookup[games] = outcome
-
+let input = open("input.txt")
+var
+    lookup = {"A X" : 3+1,"A Y":6+2,"A Z":0+3,"B X":0+1,"B Y":3+2,"B Z":6+3,"C X":6+1,"C Y":0+2,"C Z":3+3}.toTable
+    lookup2 = {"A X" : 0+3,"A Y":3+1,"A Z":6+2,"B X":0+1,"B Y":3+2,"B Z":6+3,"C X":0+2,"C Y":3+3,"C Z":6+1}.toTable
+    sum = 0
+    sum2 = 0
 
 for line in input.lines:
-  sum += lookup[line]
+    sum += lookup[line]
+    sum2 += lookup2[line]
 
 input.close()
 
 echo sum
+echo sum2
